de3e74c737c234ec27804fb1ac063a19e15fa4c5
Brian Thai <bman.brianthai@gmail.com>
2013-11-26 14:18:07 +0000

    Initial Project

---
+++ app.js
@@ -0,0 +1,20 @@
+/*
+ * This file is provided for custom JavaScript logic that your HTML files might need.
+ * Maqetta includes this JavaScript file by default within HTML pages authored in Maqetta.
+ */
+require(["dojo/ready"], function(ready){
+     ready(function(){
+         // logic that requires that Dojo is fully initialized should go here
+
+     });
+});
+
+function Home_Click()
+{
+	//Get content div
+	//Populate with data from kii
+	//First show own homes
+	
+	//Get content div
+	//Populate with 
+}
